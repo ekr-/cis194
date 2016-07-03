9a3aa44d67ce531580f6e704268bf29aea4523f5
ekr <cr.lonhard@gmail.com>
2016-07-03 12:26:07 +0300

    hw4 done

--- hw4/HW04.hs
+++ hw4/HW04.hs
@@ -1,5 +1,8 @@
+
 {-# OPTIONS_GHC -Wall #-}
 module HW04 where
+
+import Data.List
 
 newtype Poly a = P [a] deriving (Eq)
 
@@ -24,45 +27,66 @@ showHelper (x:xs) exp first
         prefix ++ (show x) ++ "x" ++ suffix ++ showHelper xs (exp-1) False 
   | exp == 0 = let prefix = if first then "" else " + " in
       prefix ++ (show x)
+  | otherwise =  ""
+
+showHelper [] _ _ = ""
 
 instance (Num a, Eq a, Show a) => Show (Poly a) where
   show (P lst) = showHelper (reverse lst) ((length lst) - 1) True
 
 -- Exercise 4 -----------------------------------------
 
+listPlus :: Num a => [a] -> [a] -> [a]
+listPlus (x:xs) (x2:xs2) = (x+x2):(listPlus xs xs2)
+listPlus (x:xs) [] = x:xs
+listPlus [] (x:xs) = x:xs
+listPlus [] [] = []
+
 plus :: Num a => Poly a -> Poly a -> Poly a
-plus = undefined
+plus (P l1) (P l2) = P (listPlus l1 l2)
 
 -- Exercise 5 -----------------------------------------
 
+timesHelper :: Num a => [a] -> Int -> Poly a -> [Poly a]
+timesHelper (x:xs) exp (P lst) =
+  (P ((replicate exp 0) ++ (map ((*) x) lst))) : (timesHelper xs (exp +1) (P lst))
+timesHelper [] _ _ = []
+
 times :: Num a => Poly a -> Poly a -> Poly a
-times = undefined
+times (P l1) (P l2) = foldl' (+) (P [0]) (timesHelper l1 0 (P l2))
 
 -- Exercise 6 -----------------------------------------
 
 instance Num a => Num (Poly a) where
     (+) = plus
     (*) = times
-    negate      = undefined
-    fromInteger = undefined
+    negate (P lst) = P (map negate lst) 
+    fromInteger x = P [fromInteger x]
     -- No meaningful definitions exist
     abs    = undefined
     signum = undefined
 
 -- Exercise 7 -----------------------------------------
 
+applyHelper :: Num a => [a] -> Int -> a -> a
+applyHelper (x:xs) exp val = x * val^exp + applyHelper xs (exp+1) val
+applyHelper [] _ _ = fromInteger 0
+
 applyP :: Num a => Poly a -> a -> a
-applyP = undefined
+applyP (P (x:xs)) val = applyHelper (x:xs) 0 val
 
 -- Exercise 8 -----------------------------------------
 
 class Num a => Differentiable a where
     deriv  :: a -> a
     nderiv :: Int -> a -> a
-    nderiv = undefined
+    nderiv n val 
+       | n == 0 = val
+       | n /= 0 = nderiv (n-1) (deriv val)
 
 -- Exercise 9 -----------------------------------------
 
-instance Num a => Differentiable (Poly a) where
-    deriv = undefined
+instance (Enum a, Num a) => Differentiable (Poly a) where
+    deriv (P [_]) = P [0]
+    deriv (P (x:xs)) = P (map (\x -> (fst x) * (snd x)) (zip [1..] xs))
 
